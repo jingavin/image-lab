1e674cff56e6f0f5d9c36158bf69a20fc3f842a2
Gavin <gavinjin62@gmail.com>
2024-11-14 17:30:43 -0800

    starter

---
+++ IOhandler.js
@@ -0,0 +1,63 @@
+const PNG = require("pngjs").PNG;
+const path = require("path");
+const yauzl = require("yauzl-promise"),
+  fs = require("fs"),
+  { pipeline } = require("stream/promises");
+
+/**
+ * Description: decompress file from given pathIn, write to given pathOut
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
+ * @return {promise}
+ */
+const unzip = async (pathIn, pathOut) => {
+  const zip = await yauzl.open(pathIn);
+
+  try {
+    for await (const entry of zip) {
+      if (entry.filename.endsWith("/")) {
+        await fs.promises.mkdir(`${pathOut}/${entry.filename}`, {
+          recursive: true,
+        });
+      } else {
+        const readStream = await entry.openReadStream();
+        const writeStream = fs.createWriteStream(
+          `${pathOut}/${entry.filename}`
+        );
+        await pipeline(readStream, writeStream);
+      }
+    }
+  } finally {
+    await zip.close();
+  }
+};
+
+const input = path.join(__dirname, "myfile.zip");
+const output = path.join(__dirname, "unzipped");
+
+unzip(input, output);
+
+/**
+ * Description: read all the png files from given directory and return Promise containing array of each png file path
+ *
+ * @param {string} path
+ * @return {promise}
+ */
+const readDir = (dir) => {};
+
+/**
+ * Description: Read in png file by given pathIn,
+ * convert to grayscale and write to given pathOut
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
+const grayScale = (pathIn, pathOut) => {};
+
+module.exports = {
+  unzip,
+  readDir,
+  grayScale,
+};
